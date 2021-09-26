31d90c8d44a074ebb4fb493143d640d7cb9b85de
milanboga <23mboga@athenian.org>
2021-09-26 11:56:56 -0700

    Finished

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -5,28 +5,33 @@ public class MyMain {
     // Rotates all values in an array to the left
     // Ex. rotateLeft([1, 2, 3, 4, 5]) => [2, 3, 4, 5, 1]
     public static int[] rotateLeft(int[] arr) {
-        int[] arr2 = new int[arr.length];
+        int dex1 = arr[0];
         for(int i = 0; i < arr.length; i++){
-            for(int j = 1; j < arr.length; j++) {
-                arr2[j] = arr[i];
+            if(i == arr.length - 1) {
+                arr[i] = dex1;
             }
-        }
+            else {
+                arr[i] = arr[i + 1];
+            }
+            }
 
-        return arr2;
+        return arr;
     }
 
     // Rotates all values in an array to the right
     // Ex. rotateRight([1, 2, 3, 4, 5]) => [5, 1, 2, 3, 4]
     public static int[] rotateRight(int[] arr) {
-        int[] arr2 = new int[arr.length];
-        for(int i = 0; i < arr.length; i++) {
-            for(int j = -1; j < arr.length - 1; j++) {
-                arr2[j] = arr[i];
+        int dex5 = arr[arr.length - 1];
+        for(int i = arr.length - 1; i >= 0; i--){
+            if(i == 0) {
+                arr[0] = dex5;
+            }
+            else {
+                arr[i] = arr[i - 1];
             }
         }
-        return arr2;
+        return arr;
     }
-
     // Replaces all duplicate values with -1
     // Ex. duplicateReplacer([1, 2, 1, 2, 1, 1]) => [1, 2, -1, -1, -1, -1]
     public static int[] duplicateReplacer(int[] arr) {
